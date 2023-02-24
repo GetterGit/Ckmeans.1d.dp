024be6ebe7c72fabe05400fcbd32bf81376723aa
Alden <aldenbradford@gmail.com>
2023-02-24 14:32:21 -0500

    fix shape comparison bug

--- python/ckmeans_1d_dp.py
+++ python/ckmeans_1d_dp.py
@@ -131,7 +131,9 @@ def ckmeans(x, k=(1, 9), y=1, method="linear", estimate_k="BIC", dissimilarity="
     k_opt = 1 + cluster.max(axis=-1)
     k_index = np.broadcast_to(np.arange(k_max), k_shape)
 
-    tot_withinss = np.where(k_index < k_opt, [withinss, 0]).sum(axis=-1)
+    tot_withinss = np.where(k_index < k_opt[..., np.newaxis], [withinss, 0]).sum(
+        axis=-1
+    )
     betweenss = totss - tot_withinss
 
     return CKMeansResult(
